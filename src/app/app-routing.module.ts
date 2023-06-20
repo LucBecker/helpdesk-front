fd0a103959c5f89182fcb3f3f27ebfab5918a741
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-20 15:39:59 -0300

    add folder tecnico

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
-import { TecnicoCreateComponent } from './components/tecnico-create/tecnico-create.component';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 const routes: Routes = [
   { path: 'login', 
